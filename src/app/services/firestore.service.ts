1a2fe868dedb54c7bf1110db0d66e27b420f6be0
Deividas Kondratjevas <deividaskondgmail.com>
2025-03-21 14:06:46 +0100

    Before Angular Update

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,44 +1,44 @@
-import { inject, Injectable } from "@angular/core";
-import { collection, Firestore, addDoc } from "@angular/fire/firestore";
-import { User } from "../../models/user.class";
+import { inject, Injectable, Injector } from '@angular/core';
+import { collection, Firestore, addDoc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.class';
+import { runInInjectionContext } from '@angular/core';  // Import the standalone function
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-
 export class FirestoreService {
+  private firestore = inject(Firestore);
+  private injector = inject(Injector);
 
-    firestore = inject(Firestore)
+  constructor() {}
 
+  async addUser(user: User): Promise<string | undefined> {
+    return runInInjectionContext(this.injector, async () => { // Use the standalone runInInjectionContext function
+      try {
+        const usersCollection = collection(this.firestore, 'users');
+        const docRef = await addDoc(usersCollection, {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          birthDate: user.birthDate,
+          street: user.street,
+          zipCode: user.zipCode,
+          city: user.city
+        });
 
-    constructor() {
+        console.log('User added with ID:', docRef.id);
+        return docRef.id;
+      } catch (error) {
+        console.error('Error adding user:', error);
+        return undefined;
+      }
+    });
+  }
 
-    }
-    async addUser(user: User): Promise<string | undefined> {
-        try {
-          const usersCollection = collection(this.firestore, 'users');
-          const docRef = await addDoc(usersCollection, {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            birthDate: user.birthDate,
-            street: user.street,
-            zipCode: user.zipCode,
-            city: user.city
-          });
-    
-          console.log('User hinzugefügt mit ID:', docRef.id);
-          return docRef.id;
-        } catch (error) {
-          console.error('Fehler beim Hinzufügen des Benutzers:', error);
-          if (error instanceof Error) {
-            console.error('Fehlermeldung:', error.message);
-          }
-          return undefined;
-        }
-    }
-
-
-
-
-
+  getUsers(): Observable<User[]> {
+    return runInInjectionContext(this.injector, () => { 
+      const usersCollection = collection(this.firestore, 'users');
+      return collectionData(usersCollection, { idField: 'id' }) as Observable<User[]>;
+    });
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule, MatDialog} from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { FirestoreService } from '../services/firestore.service';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-user',
-  imports: [MatButtonModule, MatIconModule,MatTooltipModule, MatDialogModule, FormsModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, FormsModule, MatCardModule, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
   user = new User();
+  allUsers: User[] = [];
+  
 
 
-  constructor(public dialog: MatDialog) {
-    
+  constructor(public dialog: MatDialog, public firestoreService: FirestoreService) { }
+
+  ngOnInit(): void {
+    this.firestoreService.getUsers().subscribe((changes) => {
+      console.log('Received changes from DB', changes);
+      this.allUsers = changes;
+    });
   }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
-
 }
+
